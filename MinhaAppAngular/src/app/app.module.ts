fd910a824b5628631382c4d84e5686f815c71696
JoaoVGStahl <jvitorstahl@hotmail.com>
2021-12-28 14:59:22 -0300

    First Component

--- MinhaAppAngular/src/app/app.module.ts
+++ MinhaAppAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuComponenteComponent } from './components/meu-componente/meu-componente.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuComponenteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ MinhaAppAngular/src/app/components/meu-componente/meu-componente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-componente',
+  templateUrl: './meu-componente.component.html',
+})
+export class MeuComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+      
+  }
+
+}
